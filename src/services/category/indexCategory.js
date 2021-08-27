0c01b4e6c226ed3846326ed853781f02d211d7c6
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-08-27 17:22:46 +0100

    get by id

--- src/services/category/indexCategory.js
+++ src/services/category/indexCategory.js
@@ -5,12 +5,36 @@ const categoriesRouter = express.Router()
 
 const { Category } = db
 
-categoriesRouter.route("/").get(async (req, res, next) => {
+categoriesRouter
+  .route("/")
+  .get(async (req, res, next) => {
+    try {
+      const getAll = await Category.findAll()
+
+      res.send(getAll)
+    } catch (error) {
+      next(error)
+    }
+  })
+  .post(async (req, res, next) => {
+    const createOneCategory = await Category.create(req.body)
+
+    res.send(createOneCategory)
+  })
+
+categoriesRouter.route("/:id").get(async (req, res, next) => {
   try {
-    console.log("HELLO WORLD")
+    const getOneCat = await Category.findByPk(req.params.id)
+
+    if (getOneCat === null) {
+      res
+        .status(404)
+        .send(`The category with the id ${req.params.id} was not found`)
+    } else {
+      console.log(category instanceof Category)
+    }
   } catch (error) {
     next(error)
   }
 })
-// categoriesRouter.get("/", async (req, res, next) => {})
 export default categoriesRouter
